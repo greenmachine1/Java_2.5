6bf3625893841c54b87c29d0b46e51a34202c303
greenmachine1 <scary_penguins05@hotmail.com>
2013-10-02 14:32:51 -0700

    making my service work

--- Week_1/src/com/Cory/week_1/MainActivity.java
+++ Week_1/src/com/Cory/week_1/MainActivity.java
@@ -3,10 +3,16 @@ package com.Cory.week_1;
 import com.Cory.lib.WebInfo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -26,8 +32,31 @@ public class MainActivity extends Activity {
         
         Log.i("Connection type", connectionType);
         
+        Button goButton = (Button)findViewById(R.id.goButton);
+        goButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent myIntent = new Intent(v.getContext(), JSONWeatherService.class);
+				
+				myIntent.putExtra("key", "Yes");
+			}
+        	
+        });
         
-        
+        final Handler serviceHandler = new Handler(){
+        	public void handleMessage(Message message){
+        		
+        		Object returnedObject = message.obj;
+        		
+        		if(message.arg1 == RESULT_OK && returnedObject != null){
+        			
+        			// do stuff here
+        		}
+        	}
+        };
         
         
         

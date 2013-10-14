40728053cf76e52b57a9eff2b1a07cff06787ee2
greenmachine1 <scary_penguins05@hotmail.com>
2013-10-14 01:26:31 -0700

    added my week 3 project

---
+++ Week_3/src/com/Cory/week_2/CollectionProvider.java
@@ -0,0 +1,152 @@
+package com.Cory.week_2;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class CollectionProvider extends ContentProvider{
+
+	public static final String AUTHORITY = "com.Cory.week_2.CollectionProvider";
+	
+	public static class weatherData implements BaseColumns {
+		
+		
+		/*  I believe i will have to change the /items to something witin my json string*/
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/items");
+		
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.Cory.week_2.item";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.Cory.week_2.item";
+	
+		/* Define Columns */
+		public static final String NAME_COLUMN = "Name";
+		public static final String WEATHER_COLUMN = "Weather";
+		public static final String SPEED_COLUMN = "Speed";
+		
+		public static final String[] PROJECTION = {"_Id", NAME_COLUMN, WEATHER_COLUMN, SPEED_COLUMN};
+		
+		/* constructor */ 
+		private weatherData() {};
+	
+	}
+	
+	public static final int ITEMS = 1;
+	public static final int ITEMS_ID  = 2;
+	
+	private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	
+	static {
+		uriMatcher.addURI(AUTHORITY, "items/", ITEMS);
+		uriMatcher.addURI(AUTHORITY, "items/", ITEMS_ID);
+	}
+	
+	
+	
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getType(Uri uri){
+		// TODO Auto-generated method stub
+		
+		switch (uriMatcher.match(uri)){
+		case ITEMS:
+			return weatherData.CONTENT_TYPE;
+		
+		case ITEMS_ID:
+			return weatherData.CONTENT_ITEM_TYPE;
+		
+		}
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri arg0, ContentValues arg1) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		
+		MatrixCursor result = new MatrixCursor(weatherData.PROJECTION);
+		
+		String JSONString = FileManager.getInstance().readStringFile(getContext(), MainActivity.FILE_NAME);
+		JSONObject job = null;
+		JSONArray recordArray = null;
+		JSONObject field = null;
+		
+		try{
+			job = new JSONObject(JSONString);
+			recordArray = job.getJSONArray("weather");
+			
+		} catch (JSONException e){
+			e.printStackTrace();
+		}
+		
+		if(recordArray == null){
+			return result;
+		}
+		
+		
+		switch (uriMatcher.match(uri)){
+		case ITEMS:
+			//return weatherData.CONTENT_TYPE;
+		
+			/* will be putting my results into the result matrix */
+			try{
+				/* defining the object field */  
+				/*
+				String nameString = job.getString("name");
+
+				String windSpeed = job.getJSONObject("wind").getString("speed");
+				
+				field = recordArray.getJSONObject(0).getJSONObject("weather");
+				*/
+				
+				String weatherString = recordArray.getJSONObject(0).getString("main");
+				
+				String nameString = job.getString("name");
+
+				String windSpeed = job.getJSONObject("wind").getString("speed");
+				
+				result.addRow(new Object[] { 0 + 1, nameString, weatherString, windSpeed});
+				
+			}catch (JSONException e){
+				e.printStackTrace();
+			}
+			
+		case ITEMS_ID:
+			//return weatherData.CONTENT_ITEM_TYPE;
+		
+		}
+		
+		return result;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Week_3/src/com/Cory/week_2/MainActivity.java
@@ -0,0 +1,190 @@
+/*
+ * Project		Week_2
+ * 
+ * Package		com.Cory.week_2
+ * 
+ * @Author		Cory Green
+ * 
+ * Date			Oct 9, 2013
+ */
+package com.Cory.week_2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.Cory.lib.WebInfo;
+import com.Cory.week_3.R;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.app.Activity;
+import android.app.Fragment.SavedState;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	// global variables
+	Context _context;
+	EditText userInputBox;
+	TextView text;
+    ListView listView;
+	
+	FileManager m_file;
+	String fileName = "json_info.txt";
+	
+	public static String FILE_NAME = "json_info.txt";
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        
+        
+        
+        setContentView(R.layout.main);
+        
+        /* making sure to provide the context */
+        _context = this;
+        
+        text = (TextView)findViewById(R.id.resultText);
+        
+ 
+        /* creating a singleton out of my WebInfo class */
+        WebInfo.getInstance();
+		String connectionType = WebInfo.getConnectionType(_context);
+		
+        
+        Log.i("Connection type", connectionType);
+        
+        /* targetting my listView */
+        listView = (ListView)this.findViewById(R.id.list);
+        View listHeader = this.getLayoutInflater().inflate(R.layout.list_header, null);
+        listView.addHeaderView(listHeader);
+        
+        userInputBox = (EditText)findViewById(R.id.userInput);
+        //userInputBox.setText(CollectionProvider.weatherData.CONTENT_URI.toString());
+        
+        
+        
+        
+       
+        Button goButton = (Button)findViewById(R.id.goButton);
+        goButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				
+				/* converts the user input into a string.  This will need to check
+				* for whitespace and also if the user has not inputted anything */
+				String userInputString = userInputBox.getText().toString();
+				
+				final Handler JsonHandler = new Handler(){
+
+					@Override
+					public void handleMessage(Message message){
+						
+						/* what gets returned from the called service */
+						Object returnedObject = message.obj;
+						
+						/* casting the object to a string */
+						String returnedObjectString = returnedObject.toString();
+						
+						if(message.arg1 == RESULT_OK && returnedObject != null){
+							
+							/* calls on my FileManager class */
+					        m_file = FileManager.getInstance();
+					        m_file.writeStringFile(_context, fileName, returnedObjectString);
+
+					        displayData();
+							
+						}
+						
+					}
+		    		
+		    	};
+				
+				
+				
+		    	/* creation of my messenger to the service */
+		    	Messenger jsonMessenger = new Messenger(JsonHandler);
+		
+		    	Intent myServiceIntent = new Intent(_context, JSONWeatherService.class);
+		
+		    	/* basically this passes info to my service */
+		    	myServiceIntent.putExtra("messenger", jsonMessenger);
+		    	myServiceIntent.putExtra("key", userInputString);
+		    	startService(myServiceIntent);
+			}
+        });
+         
+    }
+    
+    /* this will parse out the saved file and present it back to the user */
+    public void displayData(){
+    	
+    	/* loading my file into a string */
+    	String JSONString = m_file.readStringFile(this, fileName);
+    	Log.i("response", JSONString);
+    	
+    	ArrayList<HashMap<String, String>>mylist = new ArrayList<HashMap<String,String>>();
+    	JSONObject job = null;
+    	JSONArray results = null;
+    	
+    		/* getting the array from the field "results" */
+    		try {
+				job = new JSONObject(JSONString);
+				results = job.getJSONArray("weather");
+				
+				/* setting up the different strings to look for in the json object */
+				String weatherString = results.getJSONObject(0).getString("main");
+				
+				String nameString = job.getString("name");
+
+				String windSpeed = job.getJSONObject("wind").getString("speed");
+				
+	    		HashMap<String, String> displayMap = new HashMap<String, String>();
+	    		displayMap.put("name", nameString);
+	    		displayMap.put("weather", weatherString);
+	    		displayMap.put("speed", windSpeed);
+	    		
+	    		mylist.add(displayMap);
+	    		
+	    		/* this is complicated but it basically assigns the rows for each element */
+	    		SimpleAdapter adapter = new SimpleAdapter(this, mylist, R.layout.list_row, 
+	    				new String[] {"name", "weather", "speed"}, 
+	    				new int[] {R.id.name, R.id.weather, R.id.speed});
+	    		
+	    		
+	    		listView.setAdapter(adapter);
+				
+			} catch (JSONException e) {
+
+				e.printStackTrace();
+			}
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Inflate the menu; this adds items to the action bar if it is present. */
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
